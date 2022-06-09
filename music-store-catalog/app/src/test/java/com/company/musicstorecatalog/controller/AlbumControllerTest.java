e4e9814ecb92fe0dfd7cce5dbea076e0fcb93fc6
Roman <alonzofroman@gmail.com>
2022-06-09 08:02:58 -0700

    Added junit to dependencies

--- music-store-catalog/app/src/test/java/com/company/musicstorecatalog/controller/AlbumControllerTest.java
+++ music-store-catalog/app/src/test/java/com/company/musicstorecatalog/controller/AlbumControllerTest.java
@@ -1,5 +1,29 @@
 package com.company.musicstorecatalog.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
 public class AlbumControllerTest {
 }
